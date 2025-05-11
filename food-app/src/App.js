c800ce25af3b2166b6b35aed8a0c60b3fa4fe3f3
C-KALAGE <geet.kalani.consultant@consumer.org>
2025-05-11 21:14:20 +0530

    code push

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -3,7 +3,7 @@ import Header from "./component/Header";
 import Body from "./component/Body";
 import About from "./component/About";
 import '../index.css';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const App = () => {
     return (
